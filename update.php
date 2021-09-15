0a9500836480816e8aeaf8f036c7726b655b98cb
Rafael Sanchez <rafael@Rafaels-MacBook-Pro.local>
2021-09-15 19:36:03 -0400

    added update api

---
+++ update.php
@@ -0,0 +1,56 @@
+<?php
+	$inData = getRequestInfo();
+
+  $FirstName=$inData["FirstName"];
+  $LastName=$inData["LastName"];
+  $Email=$inData["Email"];
+  $PhoneNumber=["PhoneNumber"];
+	$ID = $inData["ID"];
+
+$conn = new mysqli("localhost", "DBADMIN", "DBADMIN", "ContactBook");
+	if ($conn->connect_error)
+	{
+		returnWithError( $conn->connect_error );
+	}
+	else
+	{
+		$stmt = $conn->prepare(" UPDATE ContactBook.Contacts
+      SET
+        FirstName=?,
+        LastName=?,
+        Email=?,
+        PhoneNumber=?,
+        where ID=?"
+    ) VALUES (
+        ?, ?, ?, ?, ?,
+    ));
+		$stmt->bind_param("ssssi",
+        $body["FirstName"],
+        $body["LastName"],
+        $body["Email"],
+        $body["PhoneNumber"],
+        $body["ID"]);
+		$stmt->execute();
+		$stmt->close();
+		$conn->close();
+		returnWithError("");
+	}
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+
+?>
