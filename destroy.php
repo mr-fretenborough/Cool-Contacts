1d2520d63940a0e88c9d5eea6549f76c233752af
sanchez-boop <72105504+sanchez-boop@users.noreply.github.com>
2021-09-16 00:34:20 -0400

    Update destroy.php

--- destroy.php
+++ destroy.php
@@ -1,54 +1,69 @@
 <?php
-
- 	$inData = getRequestInfo();
-
-   $FirstName=$inData["FirstName"];
-   $LastName=$inData["LastName"];
-   $Email=$inData["Email"];
-   $PhoneNumber=["PhoneNumber"];
- 	$ID = $inData["ID"];
-
- $conn = new mysqli("localhost", "DBADMIN", "DBADMIN", "ContactBook");
- 	if ($conn->connect_error)
- 	{
- 		returnWithError( $conn->connect_error );
- 	}
- 	else
- 	{
- 		$stmt = $conn->prepare(" UPDATE ContactBook.Contacts
-       SET
-         FirstName=?,
-         LastName=?,
-         Email=?,
-         PhoneNumber=?,
-         WHERE ID=?"
-     ) ;
- 		$stmt->bind_param("ssssi",
-         $inData["FirstName"],
-         $inData["LastName"],
-         $inData["Email"],
-         $inData["PhoneNumber"],
-         $inData["ID"]);
- 		$stmt->execute();
- 		$stmt->close();
- 		$conn->close();
- 		returnWithError("");
- 	}
+     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 
- 	function getRequestInfo()
- 	{
- 		return json_decode(file_get_contents('php://input'), true);
- 	}
+         // Grab the data and initiate the connection
+         $body = json_decode(file_get_contents('php://input'), true);
 
- 	function sendResultInfoAsJson( $obj )
- 	{
- 		header('Content-type: application/json');
- 		echo $obj;
- 	}
+         echo $body;
 
- 	function returnWithError( $err )
- 	{
- 		$retValue = '{"error":"' . $err . '"}';
- 		sendResultInfoAsJson( $retValue );
- 	}
+         $conn = new mysqli("localhost", "DBADMIN", "DBADMIN", "ContactBook");
+
+         /*
+         The input is to be of the form:
+             {
+                 "FirstName":"",
+                 "LastName":"",
+                 "Email":"",
+                 "PhoneNumber":"",
+                 "UserID":""
+             }
+         The output will be of the form:
+             {
+                 "status":""
+             }
+         */
+
+         // Validate connection
+         if ($conn->connect_error) {
+             returnStatus($conn->connect_error);
+         } else {
+             // Seed the SQL Query with basic format
+             $query = $conn.prepare(" DELETE FROM Contacts WHERE FirstName = ? ");
+             // Grab JSON and populate the SQL Query
+             $query->bind_param("s", $body["FirstName"]);
+             // Execute the SQL Query
+             $query->execute();
+
+             // Status will be the number of rows deleted. This should be either a 1 for success or 0 for failure
+             $status = $query->get_result()->fetch_assoc();
+
+             // Report the status
+             if ($status == 1) {
+                 returnStatus("SUCCESS: Contact deletion completed successfully.");
+             } elseif ($status == 0) {
+                 returnStatus("ERROR: No contacts were deleted.");
+             } else {
+                 returnStatus("FAILURE: More than one contact was deleted.");
+             }
+         }
+
+         // Let 'em go
+         $query->close();
+         $conn->close();
+
+         returnStatus("just testing this.");
+     }
+
+     function returnJSON( $object )
+     {
+         header('Content-type: application/json');
+         echo $object;
+     }
+
+     function returnStatus( $status )
+     {
+         $format = '{"status":"' . $status . '"}';
+         returnJSON( $format );
+     }
+ ?>
