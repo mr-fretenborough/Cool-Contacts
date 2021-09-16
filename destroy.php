a88e1e54fb6d45dfdc6fc3f176b158005ec36977
kier fretenborouh <mr.fretenborough@outlook.com>
2021-09-16 17:21:39 -0400

    preparation check

--- destroy.php
+++ destroy.php
@@ -1,10 +1,17 @@
 <?php
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        // Initialize some variables
+        $server = "localhost";
+        $username = "root";
+        $password = "TMs7aedJuMNX";
+        $database = "ContactBook";
+
+
         // Grab the data and initiate the connection
         $body = json_decode(file_get_contents('php://input'), true);
-        $conn = new mysqli("localhost", "root", "TMs7aedJuMNX", "ContactBook");
+        $conn = new mysqli($server, $username, $password, $database);
 
-         /*
+         /**************************************
          The input is to be of the form:
              {
                  "FirstName":"",
@@ -17,7 +24,7 @@
              {
                  "status":""
              }
-         */
+         ***************************************/
 
          // Validate connection
          if ($conn->connect_error) {
@@ -25,10 +32,17 @@
          } else {
              // Seed the SQL Query with basic format
              $query = $conn.prepare(" DELETE FROM Contacts WHERE FirstName = ? ");
+
+             // Check if the query preparation completed successfully
+             if ( false === $query ) {
+                returnStatus('FAILURE: Query preparation failed.');
+                exit();
+            }
+
              // Grab JSON and populate the SQL Query
-             $query->bind_param("s", $body["FirstName"]);
+             $bind = $query->bind_param("s", $body["FirstName"]);
              // Execute the SQL Query
-             $query->execute();
+             $exec = $query->execute();
 
              // Status will be the number of rows deleted. This should be either a 1 for success or 0 for failure
              $status = $query->get_result()->fetch_assoc();
