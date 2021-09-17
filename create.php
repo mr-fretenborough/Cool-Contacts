9d3d37cda1a3cb2cd780ef1cc61deec7c541eb65
kier fretenborouh <mr.fretenborough@outlook.com>
2021-09-17 03:47:26 -0400

    update create.php

--- create.php
+++ create.php
@@ -1,53 +1,74 @@
 <?php
-    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        // Grab the data and initiate the connection
+        /**************************************
+             The input is to be of the form:
+                {
+                    "FirstName":"",
+                    "LastName":"",
+                    "Email":"",
+                    "PhoneNumber":"",
+                    "UserID":""
+                }
+            The output will be of the form:
+                {
+                    "status":""
+                }
+        ***************************************/
+
+
+    if ($_SERVER['REQUEST_METHOD']==='POST') {
+        // Pull the JSON object from the POST request
         $body = json_decode(file_get_contents('php://input'), true);
+
+        // Parse the JSON object
+        $first = $body["FirstName"];
+        $last = $body["LastName"];
+        $email = $body["Email"];
+        $number = $body["PhoneNumber"];
+        $user = $body["UserID"];
+
+        // Establish the connection to the MySQL Instance
         $conn = new mysqli("localhost", "DBADMIN", "DBADMIN", "ContactBook");
 
-        // Validate connection
-        if ($conn->connect_error) {
-            returnStatus($conn->connect_error);
+        // Validate the connection
+        if ( $conn->connect_error ) {
+            returnStatus( $conn->connect_error );
         } else {
-            // Seed the SQL Query with basic format
-            $query = $conn.prepare(" INSERT INTO ContactBook.Contacts
-                FirstName,
-                LastName,
-                Email,
-                PhoneNumber,
-                UserID
-            ) VALUES (
-                ?, ?, ?, ?, ?
-            )");
-            // Grab JSON and populate the SQL Query
-            $query->bind_param("ssssi",
-                $body["FirstName"],
-                $body["LastName"],
-                $body["Email"],
-                $body["PhoneNumber"],
-                $body["UserID"]
-            );
-            // Execute the SQL Query
+            // Create the query template
+            $query = $conn->prepare(" INSERT INTO ContactBook.Contacts
+                    FirstName,
+                    LastName,
+                    Email,
+                    PhoneNumber,
+                    UserID
+                ) VALUES (
+                    ?, ?, ?, ?, ?
+                )
+            ");
+
+            // Bind the parameters from the JSON object to the SQL query
+            $query->bind_param("ssssi", $first, $last, $email, $number, $user);
+
+            // Execute the SQL query
             $query->execute();
-            // Status will be the number of rows deleted. This should be either a 1 for success or 0 for failure
-            $status = $query->get_result()->fetch_assoc();
 
-            // Report the status
-            if ($status) {
-                returnStatus("SUCCESS: Contact creation completed successfully.");
+            // Grab the result. This should be the number of rows inserted
+            $inserted = $conn->insert_id;
+
+            if ($inserted) {
+                returnStatus("SUCCESS: Insertion successful.");
             } else {
-                returnStatus("FAILURE: Contact creation failed to succeed.");
+                returnStatus("ERROR: Insertion unsuccessful.");
             }
-        }
 
-        // Let 'em go
-        $query->close();
-        $conn->close();
+            $query->close();
+            $conn->close();
+        }
     }
 
-    function returnStatus($msg) {
-        $format = '{"status":"' . $msg . '"}';
-        // Report the status of the operation
+    function returnStatus( $message )
+    {
+        $object = '{"status":"' . $message . '"}';
         header('Content-type: application/json');
-        echo $format;
+        echo $object;
     }
 ?>
